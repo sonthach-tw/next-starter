37b4213f182f67cb8b62c3bd5afefd1363f22196
Son Thach <son.thach@thoughtworks.com>
2023-05-29 21:29:18 +0700

    Remove auth error

--- app/firebase.js
+++ app/firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -14,7 +13,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 const firestore = getFirestore(app);
 
-export { app, auth, firestore };
+export { app, firestore };
